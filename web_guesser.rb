87dc5514805f96e3368d2b35b47d88ff655c2630
elbohairy <totski@gmail.com>
2017-06-28 13:04:37 -0400

    colorized output

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,22 +4,24 @@ require 'sinatra/reloader'
 number ||= rand 101
 
 def message num, guess
+  guess = guess.to_i
   if guess > (num + 5)
-    "Way too high!"
+    ["Way too high!", "body { background: #f00; }"]
   elsif guess > num
-    "Too high!"
+    ["Too high!", "body { background: #fcc; }"]
   elsif guess == num
-    "You got it right!<br />The SECRET NUMBER is #{num}"
+    ["You got it right!<br />The SECRET NUMBER is #{num}",
+      "body { background: #0c0; }"]
   elsif guess < (num - 5)
-    "Way too low!"
+    ["Way too low!", "body { background: #f00; }"]
   elsif guess < num
-    "Too low!"
+    ["Too low!", "body { background: #fcc; }"]
   end
 end
 
 
 get '/' do
-  user_guess = params['guess'].to_i
+  user_guess = params['guess']
   response = message number, user_guess
   erb :index, :locals => {:number => number, :user_guess => user_guess, :message => response}
 end
